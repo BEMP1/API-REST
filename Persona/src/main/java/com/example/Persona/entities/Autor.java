5547cf1f863b0c555234f6c99bf460b6f351b5e4
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2024-09-22 01:17:04 -0300

    Add files via upload

---
+++ Persona/src/main/java/com/example/Persona/entities/Autor.java
@@ -0,0 +1,22 @@
+package com.example.Persona.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+@Audited
+@Table
+public class Autor {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String biografia;
+}
